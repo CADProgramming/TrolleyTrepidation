3088d6cd5a46ab129000d2f88d5f3e879e2b1520
Blake Chalmers (1000064560) <CHALBR1@student.op.ac.nz>
2021-03-18 14:52:04 +1300

    pulling updated work on player movement

---
+++ TrolleyTrepidation/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private HashSet<GameObject> colliders = new HashSet<GameObject>();
+    public HashSet<GameObject> GetColliders() { return colliders; }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        colliders.Add(other.gameObject);
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- TrolleyTrepidation/Assets/Scripts/World Objects/TrolleyScripts/TrolleyHandleTrigger.cs
+++ TrolleyTrepidation/Assets/Scripts/World Objects/TrolleyScripts/TrolleyHandleTrigger.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class TrolleyHandleTrigger : MonoBehaviour
 {
-   
+
+    public GameObject robot;
+    private PlayerController script;
+    
+
 
     public void OnTriggerEnter(Collider other)
     {
-        transform.parent = other.gameObject.transform.parent;
+        if (other.transform.root.gameObject.name == robot.name)
+        {
+  //          if(gameObject.Refe)
+ //           if (script.GetColliders().Contains(gameObject))
+ //           {
+                transform.SetParent(other.transform.root);
+ //           }
+        }
+        
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        script = robot.GetComponent<PlayerController>();
+
     }
 
     // Update is called once per frame
