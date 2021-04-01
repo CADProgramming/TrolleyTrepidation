4fc597ee658ce4ce504ba88ae2e9abb1d5e183f4
Blake Chalmers (1000064560) <CHALBR1@student.op.ac.nz>
2021-04-01 14:51:08 +1300

    almost finished work on spawning and grabbing multiple trolleys

--- TrolleyTrepidation/Assets/Scripts/Player/PlayerController.cs
+++ TrolleyTrepidation/Assets/Scripts/Player/PlayerController.cs
@@ -4,46 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject spawnController;
+    private SpawnController trolleySpawner;
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
     public bool carrying;
-    
+    public float distance = 1.3F;
 
     // Use this for initialization
     void Start()
     {
+        trolleySpawner = spawnController.GetComponent<SpawnController>();
         item.GetComponent<Rigidbody>().useGravity = true;
     }
     // Update is called once per frame
     void Update()
     {
-        if (carrying == false)
+        foreach(GameObject trolley in trolleySpawner.arrayTrolleys)
         {
-            if (Input.GetKeyDown(KeyCode.K) && (guide.transform.position - item.transform.position).sqrMagnitude < 2)
-            {
-                pickup();
-                carrying = true;
-            }
+            if (Input.GetKeyDown(KeyCode.K) && (guide.transform.position - trolley.transform.position).sqrMagnitude < 2)
+                {
+                    pickup(trolley);
+                    carrying = true;
+                }
+            
+            
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    drop(trolley);
+                    carrying = false;
+                }
+            
         }
-        else if (carrying == true)
-        {
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                drop();
-                carrying = false;
-            }
-        }
+        
     }
-    void pickup()
+    void pickup(GameObject trolley)
     {        
-        item.transform.position = guide.transform.localPosition + guide.transform.rotation * Vector3.forward;
-        item.transform.rotation = guide.transform.localRotation;
-        item.transform.parent = tempParent.transform;
+        trolley.transform.position = guide.transform.localPosition + guide.transform.rotation * new Vector3 (0,0,distance);
+        trolley.transform.rotation = guide.transform.localRotation;
+        trolley.transform.parent = tempParent.transform;
     }
-    void drop()
+    void drop(GameObject trolley)
     {        
-        item.transform.parent = null;
-        item.transform.position = guide.transform.position;
+        trolley.transform.parent = null;
+        trolley.transform.position = guide.transform.position;
     }
 }
--- TrolleyTrepidation/Assets/Scripts/World Objects/SpawnController.cs
+++ TrolleyTrepidation/Assets/Scripts/World Objects/SpawnController.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.other.gameObject.name.Contains("Trolley"))
-        {
-            Destroy(collision.other.gameObject);
-        }
-        
-    }
+    public GameObject myPrefab;
+    public Transform location;
+    public GameObject[] arrayTrolleys;
     // Start is called before the first frame update
     void Start()
     {
+        arrayTrolleys = new GameObject[51];
+        for(int i = 0; i < 3; i++)
+        {
+            arrayTrolleys[i] = Instantiate(myPrefab, SpawnRandomLocation(), Quaternion.identity);
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+    }
+    Vector3 SpawnRandomLocation()
+    {
+        return new Vector3(Random.Range(location.position.x - (location.localScale.x / 2), location.position.x + (location.localScale.x /2)), 0, Random.Range(location.position.z - (location.localScale.z /2), location.position.z + (location.localScale.z /2)));
     }
 }
