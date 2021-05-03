1d1f6edeb73ca59cd0ce097069e1cbe09161cf33
Blake Chalmers (1000064560) <CHALBR1@student.op.ac.nz>
2021-05-03 14:37:00 +1200

    pushing work on movement with trolleys

--- TrolleyTrepidation/Assets/Scripts/Player/PlayerMovementNew.cs
+++ TrolleyTrepidation/Assets/Scripts/Player/PlayerMovementNew.cs
@@ -11,6 +11,7 @@ public class PlayerMovementNew : MonoBehaviour
     public List<Transform> bodyParts = new List<Transform>();
 
     public float minDistance = 0.25f;
+    public float distance = 1.3F;
 
     public int beginSize;
 
@@ -36,76 +37,48 @@ public class PlayerMovementNew : MonoBehaviour
     }
     public void OnTriggerEnter(Collider other)
     {
-        if (other.name.Contains("Trolley"))
+        if(bodyParts.Contains(other.transform) == false)
         {
-            Destroy(other.gameObject);
-            AddBodyPart();
-        }        
+            if (other.name.Contains("Trolley"))
+            {
+                
+                AddBodyPart(other.gameObject);
+            }
+        }              
     }
 
     private void MovePlayer()
     {
+        
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            playerBody.rotation = Quaternion.Euler(0, -90, 0);
+            bodyParts[bodyParts.Count - 1].rotation = Quaternion.Euler(0, -90, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            playerBody.rotation = Quaternion.Euler(0, 90, 0);
+            bodyParts[bodyParts.Count - 1].rotation = Quaternion.Euler(0, 90, 0);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            playerBody.rotation = Quaternion.Euler(0, 0, 0);
+            bodyParts[bodyParts.Count - 1].rotation = Quaternion.Euler(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            playerBody.rotation = Quaternion.Euler(0, 180, 0);
+            bodyParts[bodyParts.Count - 1].rotation = Quaternion.Euler(0, 180, 0);
         }
 
-        playerBody.velocity = transform.rotation * (MOVE_SPEED * Vector3.forward);
+        bodyParts[bodyParts.Count - 1].GetComponent<Rigidbody>().velocity = transform.rotation * (MOVE_SPEED * Vector3.forward);
         moveTrolley();
     }
     private void moveTrolley()
     {
         float curspeed = speed;
-        for (int i = 1; i < bodyParts.Count; i++)
-        {
 
-            curBodyPart = bodyParts[i];
-            PrevBodyPart = bodyParts[i - 1];
-
-            dis = Vector3.Distance(PrevBodyPart.position, curBodyPart.position);
-
-            Vector3 newpos = PrevBodyPart.position;
-
-            newpos.y = bodyParts[0].position.y;
-
-            float T = Time.deltaTime * dis / minDistance * curspeed;
-
-            if (T > 0.5f)
-                T = 0.5f;
-            curBodyPart.position = Vector3.Slerp(curBodyPart.position, newpos, T);
-            curBodyPart.rotation = Quaternion.Slerp(curBodyPart.rotation, PrevBodyPart.rotation, T);
-
-        }
+        
     }
-    public void AddBodyPart()
+    public void AddBodyPart(GameObject trolley)
     {
-        if (bodyParts.Count == 1)
-        {
-            Transform newpart = (Instantiate(bodyprefabs, transform.position., transform.rotation) as GameObject).transform;
-            newpart.SetParent(transform);
-
-            bodyParts.Add(newpart);
-        }
-        else 
-        {
-            Transform newpart = (Instantiate(bodyprefabs, bodyParts[bodyParts.Count - 1].position, bodyParts[bodyParts.Count - 1].rotation) as GameObject).transform;
-            newpart.SetParent(transform);
-            bodyParts.Add(newpart);
-        }
-
+        //Transform newpart = (Instantiate(bodyprefabs, bodyParts[bodyParts.Count - 1].transform.position + new Vector3(0,0,distance+bodyParts.Count - 1), bodyParts[bodyParts.Count -1 ].rotation) as GameObject).transform;
+        bodyParts.Add(trolley.transform);     
     }
-
-
 }
